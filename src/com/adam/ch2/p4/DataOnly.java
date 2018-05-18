5e63692b46173182708e1870edff8a03ee9fa402
adam <starksmile@hotmail.com>
2018-05-18 10:01:27 +0800

    ch2 p4

---
+++ src/com/adam/ch2/p4/DataOnly.java
@@ -0,0 +1,13 @@
+package com.adam.ch2.p4;
+
+/**
+ * @author adam
+ * 创建于 2018-05-18 09:59.
+ */
+public class DataOnly {
+
+    int i;
+    double d;
+    boolean b;
+
+}
---
+++ src/com/adam/ch2/p4/DataOnlyMain.java
@@ -0,0 +1,14 @@
+package com.adam.ch2.p4;
+
+/**
+ * @author adam
+ * 创建于 2018-05-18 10:00.
+ */
+public class DataOnlyMain {
+    public static void main(String[] args) {
+        DataOnly data = new DataOnly();
+        data.i = 1;
+        data.d = 2.3;
+        data.b = true;
+    }
+}

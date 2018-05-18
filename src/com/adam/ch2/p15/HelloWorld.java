7d3151cd05b6d72e4780127581c5d23f95af0a90
adam <starksmile@hotmail.com>
2018-05-18 10:53:23 +0800

    ch2 p15

---
+++ src/com/adam/ch2/p15/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.adam.ch2.p15;
+
+/**
+ * @author adam
+ * 创建于 2018-05-18 10:50.
+ * javac+java编译运行。
+ * 正确的运行命令为：java -classpath /home/adam/idea/ThinkingInJava/src/ com.adam.ch2.p2.HelloWorld。
+ * 注意配置CLASSPATH。
+ */
+public class HelloWorld {
+
+    /**
+     * Main application entry point.
+     * @param args command line parameters
+     */
+    @Deprecated
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}

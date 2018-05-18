0bd4edff1e054ddb12245acef7e84699c14b841d
adam <starksmile@hotmail.com>
2018-05-18 09:59:04 +0800

    ch2 p3

---
+++ src/com/adam/ch2/p3/ATypeName.java
@@ -0,0 +1,9 @@
+package com.adam.ch2.p3;
+
+/**
+ * @author adam
+ * 创建于 2018-05-18 09:57.
+ */
+public class ATypeName {
+    /* Class body goes here */
+}
---
+++ src/com/adam/ch2/p3/ATypeNameMain.java
@@ -0,0 +1,12 @@
+package com.adam.ch2.p3;
+
+/**
+ * @author adam
+ * 创建于 2018-05-18 09:58.
+ */
+public class ATypeNameMain {
+    public static void main(String[] args) {
+        ATypeName aTypeName = new ATypeName();
+        System.out.println(aTypeName);
+    }
+}
